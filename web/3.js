2f9ffda4e50c776d53804a2662a53a7d6543aef6
Sunny W. Park <parksunwoo@Sunnys-MacBook-Pro.local>
2022-04-05 10:08:02 +0900

    220404

---
+++ web/3.js
@@ -0,0 +1,26 @@
+<!doctype html>
+<html>
+	<head>
+	<!-- Title 은 반드시!-->
+	<title>WEB - Welcome</title>
+	<meta charset="utf-8">
+	</head>
+
+<body>
+	<h1><a href="index.html">WEB</a></h1>	
+	WWW is <strong>World <u>Wide</u> WEB.</strong><br>
+	<img src="https://images.unsplash.com/photo-1648983102846-32ae9101a0ba?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt="splash" width="500" height="auto">
+
+	<ol>
+		<li><a href="1.html"> html</li>
+		<li><a href="2.css"> css</li>
+		<li><a href="3.js"> js</li>
+		<!-- <li*3 + tab> in VSCode-->
+	</ol>
+
+	<h2 id="2.html">css</h2>
+Images are not technically inserted into a web page; images are linked to web pages. The <img> tag creates a holding space for the referenced image.
+
+</body>
+</html>
+🏻
